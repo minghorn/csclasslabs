ec14a494bdc8e902357f4680a9c50e3bfe7c5722
Ming Horn <minghaihorn@gmail.com>
2015-01-21 12:28:10 -0800

    Multiplayer game logic

--- ming lab/labs/lab21_abstract_newLabs_21/1.BlackJack_TEXT/BlackJack/BlackJack.java
+++ ming lab/labs/lab21_abstract_newLabs_21/1.BlackJack_TEXT/BlackJack/BlackJack.java
@@ -5,20 +5,24 @@
 //Lab  - 
 
 import static java.lang.System.*;
+import java.util.*;
 import java.util.Scanner;
 
 public class BlackJack
 {
     private ArrayList<Playerable> players;
     private Dealer dealer = new Dealer();
+    private ArrayList<int> winners = new ArrayList<int>();
     private Scanner in = new Scanner(System.in);
 
     public BlackJack()
     {
+       players = new ArrayList<Playerable>();
+       ArrayList<int> playerTotals = new ArrayList<int>();
        System.out.println("How many players?");
        int play = in.nextInt();
        players.add(dealer);
-       for(i = 1; i < play+1; i++){
+       for(int i = 1; i < play+1; i++){
            Player player = new Player();
            players.add(player);
         }
@@ -26,35 +30,49 @@ public class BlackJack
 
     public void playGame()
     {
-        for(int i=0; i < players.size(); i++){
+        for(int i = players.size()-1; i > -1; i--){
             players.get(i).addCardToHand(dealer.deal());
             players.get(i).addCardToHand(dealer.deal());
-            System.out.println(players.get(i).getHandValue());
-            System.out.println("player " + player.get(i).toString());
+            playerTotals.add(i, players.get(i).getHandValue());
+        }
+        for(int i = players.size()-1; i > -1; i--){
+            while(i > 0 && playerTotals.get(i) <= 21 && players.get(i).hit()){
+                players.get(i).addCardToHand(dealer.deal());
+                playerTotals.get(i) = players.get(i).getHandValue();
+                System.out.println(playerTotals.get(i));
+                System.out.println("player "+ i + " " + players.get(i).toString());
+            }
+            System.out.println(playerTotals.get(0));
+            System.out.println("dealer " + dealer.toString());
+            dealer.hit();
+            while(dealer.hit()){
+                dealer.addCardToHand(dealer.deal());
+                int dealerTotal = dealer.getHandValue();
+                System.out.println(dealerTotal);
+                System.out.println("dealer " + dealer.toString());
+            }
         }
         
-        while(playerTotal <= 21 && player.hit()){
-            player.addCardToHand(dealer.deal());
-            playerTotal = player.getHandValue();
-            System.out.println(playerTotal);
-            System.out.println("player " + player.toString());
+        for(int i = 1; i < players.size(); i++){
+            System.out.println("Player " + i + " : " + playerTotals.get(i));
+            System.out.println("Player " + i + " : " + players.get(i).toString());
         }
-        System.out.println(dealerTotal);
-        System.out.println("dealer " + dealer.toString());
-        dealer.hit();
-        while(dealer.hit()){
-            dealer.addCardToHand(dealer.deal());
-            dealerTotal = dealer.getHandValue();
-            System.out.println(dealerTotal);
-            System.out.println("dealer " + dealer.toString());
+            System.out.println("Dealer : " + dealerTotal);
+            System.out.println("Dealer : " + dealer.toString());
+        for(int i = 0; i < players.size(); i++){
+            
+            int win = 0;
+            if(playerTotals.get(i) > win){
+                win = playerTotals.get(i);
+            }
+            for(int j = 0; j < players.size(); j++){
+                if(playerTotals.get(j) == win){
+                    winners.add(j);
+                }
+            }
         }
-        if(playerTotal > dealerTotal && playerTotal <= 21 || dealerTotal > 21 && playerTotal <= 21){
-            System.out.println("Player won!");
-            player.setWinCount(1);
-        }
-        else if(dealerTotal > playerTotal && dealerTotal <= 21 || playerTotal > 21 && dealerTotal <= 21){
-            System.out.println("Dealer won!");
-            dealer.setWinCount(1);
+        for(int i = 0; i < winners.size(); i++){
+            players.get(winners.get(i)).setWinCount(1);
         }
         dealer.shuffle();
         System.out.println("Player: " + player.getWinCount() + " Dealer: " + dealer.getWinCount());
