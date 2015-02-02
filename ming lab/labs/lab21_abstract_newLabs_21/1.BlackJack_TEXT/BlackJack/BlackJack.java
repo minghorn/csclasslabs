c27242fc09ec96f35de037d6af86c4184541a32f
Other <random@gmail.com>
2015-02-02 11:35:15 -0800

    multiplayer etc

--- ming lab/labs/lab21_abstract_newLabs_21/1.BlackJack_TEXT/BlackJack/BlackJack.java
+++ ming lab/labs/lab21_abstract_newLabs_21/1.BlackJack_TEXT/BlackJack/BlackJack.java
@@ -11,15 +11,12 @@ import java.util.Scanner;
 public class BlackJack
 {
     private ArrayList<Playerable> players;
-    private Dealer dealer = new Dealer();
-    private ArrayList<Integer> winners = new ArrayList<Integer>();
     private Scanner in = new Scanner(System.in);
-    private ArrayList<Integer> playerTotals;
+    private Dealer dealer = new Dealer();
 
     public BlackJack()
     {
        players = new ArrayList<Playerable>();
-       playerTotals = new ArrayList<Integer>();
        System.out.println("How many players?");
        int play = in.nextInt();
        players.add(dealer);
@@ -31,59 +28,47 @@ public class BlackJack
 
     public void playGame()
     {
+        dealer.shuffle();
         for(int i = players.size()-1; i > -1; i--){
             players.get(i).addCardToHand(dealer.deal());
             players.get(i).addCardToHand(dealer.deal());
-            playerTotals.add(i, players.get(i).getHandValue());
         }
         for(int i = players.size()-1; i > -1; i--){
-            while(i > 0 && playerTotals.get(i) <= 21 && players.get(i).hit()){
-                players.get(i).addCardToHand(dealer.deal());
-                playerTotals.get(i) = players.get(i).getHandValue();
-                System.out.println(playerTotals.get(i));
-                System.out.println("player "+ i + " " + players.get(i).toString());
-            }
-            System.out.println(playerTotals.get(0));
-            System.out.println("dealer " + dealer.toString());
-            dealer.hit();
-            while(dealer.hit()){
-                dealer.addCardToHand(dealer.deal());
-                int dealerTotal = dealer.getHandValue();
-                System.out.println(dealerTotal);
-                System.out.println("dealer " + dealer.toString());
-            }
-        }
-        
-        for(int i = 1; i < players.size(); i++){
-            System.out.println("Player " + i + " : " + playerTotals.get(i));
-            System.out.println("Player " + i + " : " + players.get(i).toString());
-        }
-            System.out.println("Dealer : " + dealerTotal);
-            System.out.println("Dealer : " + dealer.toString());
-        for(int i = 0; i < players.size(); i++){
-            
-            int win = 0;
-            if(playerTotals.get(i) > win){
-                win = playerTotals.get(i);
-            }
-            for(int j = 0; j < players.size(); j++){
-                if(playerTotals.get(j) == win){
-                    winners.add(j);
+           Player currentPlayer = players.get(i);
+           System.out.println("Player " + i + " turn");
+           System.out.println(currentPlayer.toString());
+           while(currentPlayer.getHandValue() < 21){
+               System.out.println("Hit? y or n");
+               if(in.next().equals("y")){
+                   currentPlayer.addCardToHand(dealer.deal());
+                   System.out.println(currentPlayer.toString());
                 }
             }
         }
-        for(int i = 0; i < winners.size(); i++){
-            players.get(winners.get(i)).setWinCount(1);
+        for(int i = 0; i < players.size(); i++){
+            System.out.println("Player " + i + " hand value:" + players.get(i).getHandValue());
+        }
+        Player winner = players.get(0);
+        for(int i = 0; i < players.size(); i++){
+            Player current = players.get(i);
+            if(winner.getHandValue() > 21){
+                winner = players.get(i+1);
+            }
+            if(current.getHandValue() > winner.getHandValue() && current.getHandValue() <= 21){
+                winner = current;
+            }
+        }
+        winner.setWinCount(1);
+        for(int i = 0; i < players.size(); i++){
+            System.out.println("Player "+ i + "win count: " + players.get(i).getWinCount());
         }
         dealer.shuffle();
-        System.out.println("Player: " + player.getWinCount() + " Dealer: " + dealer.getWinCount());
         System.out.println("Play again? y or n");
         String q = in.next();
         if(q.equals("y") || q.equals("Y")){
-            player.resetHand();
-            dealer.resetHand();
-            dealerTotal = dealer.getHandValue();
-            playerTotal = player.getHandValue();
+            for(int i = 0; i < players.size(); i++){
+                players.get(i).resetHand();
+            }
             this.playGame();
         }
     }
